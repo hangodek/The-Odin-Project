409c98b3a268e7d8506599be503e2eaf8957916a
Han <icyfarhan@gmail.com>
2023-12-13 21:41:52 +0700

    Completed the javascript exercise number 3

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let temp = String(word);
+    temp = temp.split('');
 
+    let joinWord = '';
+
+    for (let i = temp.length - 1; i >= 0; i--) {
+        joinWord = joinWord.concat(temp[i]);
+    }
+
+    console.log(joinWord);
+    return joinWord;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
