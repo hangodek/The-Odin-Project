e4641f060ad8cfaf5cb40bf486bf91a73da4e3a0
Han <icyfarhan@gmail.com>
2023-12-15 21:19:22 +0700

    Added documentation for exercises 4

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const myArray = [];
 
+    array.forEach((x) => {
+        if (!args.includes(x)) {
+            myArray.push(x);
+        }
+    })
+
+    console.log(myArray);
+    return myArray;
 };
+
+removeFromArray([1, 2, 3, 4, 5, 6, 7], 1);
 
 // Do not edit below this line
 module.exports = removeFromArray;
