fc0bb4ad2ccb667aefc6990b5560be0669524c95
Han <icyfarhan@gmail.com>
2023-12-16 10:14:07 +0700

    Added documentation for the exercise 5

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) {
+        return 'ERROR';
+    }
 
-};
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return 'ERROR';
+    }
 
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+
+    let mySum = 0;
+
+    for (let i = x; i <= y; i++) {
+        mySum += i;
+    }
+
+    console.log(mySum);
+    return mySum;
+}
+
+
+sumAll(2, 4);
 // Do not edit below this line
 module.exports = sumAll;
